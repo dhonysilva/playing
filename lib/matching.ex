60e14ebc33d29b54d30cc7e64e0e22fb93f497ee
Dhony Silva <dhony.web@gmail.com>
2024-12-01 17:49:41 -0300

    Order of Pattern Matching

--- lib/matching.ex
+++ lib/matching.ex
@@ -3,10 +3,10 @@ defmodule Matching do
     data = %{name: "Howard", age: 35}
 
     case data do
+      _other -> "Uhh... what's that?"
       %{name: "Howard"} -> "Yes sir Mr. Admin"
       %{name: name} -> "Greetings #{name}!"
       %{age: age} -> "I don't know who you are, but you're #{inspect(age)} years old!"
-      _other -> "Uhh... what's that?"
     end
   end
 end
