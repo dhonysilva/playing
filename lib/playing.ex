3edcecb2646df59b15fc344c5bb20065d5f46db3
Dhony Silva <dhony.web@gmail.com>
2024-12-01 15:19:02 -0300

    Start mix new playing projet for the thinkelixir pattern matching course

---
+++ lib/playing.ex
@@ -0,0 +1,18 @@
+defmodule Playing do
+  @moduledoc """
+  Documentation for `Playing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Playing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
