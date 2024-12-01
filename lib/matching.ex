ef467f4a184c8ceb2700f349760fa3b01efe02f9
Dhony Silva <dhony.web@gmail.com>
2024-12-01 18:03:19 -0300

    Deeper data matches

--- lib/matching.ex
+++ lib/matching.ex
@@ -9,4 +9,23 @@ defmodule Matching do
       %{age: age} -> "I don't know who you are, but you're #{inspect(age)} years old!"
     end
   end
+
+  def deeper_data do
+    data = %{
+      important_flag: true,
+      level_1: %{
+        other: "stuff",
+        level_2: %{
+          value: 123,
+          more: "stuff"
+        }
+      }
+    }
+
+    case data do
+      %{important_flag: false} -> {:ok, 0}
+      %{important_flag: true, level_1: %{level_2: %{value: value}}} -> {:ok, value}
+      _other -> {:error, "Invalid data"}
+    end
+  end
 end
