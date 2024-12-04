50b320e93ea2932e9de03d5a753f81ec1f29e564
Dhony Silva <dhony.web@gmail.com>
2024-12-04 18:02:08 -0300

    Naming the parameter for clarity

--- lib/testing.ex
+++ lib/testing.ex
@@ -6,9 +6,9 @@ defmodule Testing do
 
   # web_params = %{"name" => "John", "email" => "john@example.com"}
 
-  def do_work(%{"email" => email} = params) do
+  def do_work(%{"email" => email} = _customer_params) do
     IO.inspect(email)
-    IO.inspect(params)
-    "Send an email to #{email} addressed to #{params["name"]}"
+    # IO.inspect(params)
+    "Send an email to #{email}}"
   end
 end
